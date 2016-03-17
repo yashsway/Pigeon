0b71521daebc984ea243c6e22f09f34f1287d07e
Yash Gopal <reach.yashg@gmail.com>
2016-03-17 15:25:38 -0400

    Modified listing to display 60chars, modified post-report-submission text on staffPortal

--- Main/obj/reportEntry.php
+++ Main/obj/reportEntry.php
@@ -5,7 +5,11 @@ function reportEntry(){
         for($i=0;$i<count($reports);$i++){
             echo '<tr id="' . $reports[$i]->ID .'">';
             echo '<td class="report-elements report-id text ">' . htmlspecialchars($reports[$i]->ID,ENT_QUOTES,'UTF-8') . '</td>';
-            echo '<td class="report-elements report-title text">' . htmlspecialchars(substr($reports[$i]->summary,0,60),ENT_QUOTES,'UTF-8') . '</td>';
+            if(strlen($reports[$i]->summary)>=60){
+                echo '<td class="report-elements report-title text">' . htmlspecialchars(substr($reports[$i]->summary,0,60),ENT_QUOTES,'UTF-8') . '...</td>';
+            }else{
+                echo '<td class="report-elements report-title text">' . htmlspecialchars(substr($reports[$i]->summary,0,60),ENT_QUOTES,'UTF-8') . '</td>';
+            }
             echo '<td class="report-elements report-icons report-resolution">' . resolutionFlagCodeGenerator($reports[$i]->resolved) . '</td>';
             echo '<td class="report-elements report-icons report-tag">' . tagCodeGenerator($reports[$i]->tag) . '</td>';
             echo '<td class="report-elements report-date text">' . htmlspecialchars($reports[$i]->_date,ENT_QUOTES,'UTF-8') . '</td>';
